2722395910c13025d49c24fc681fa7705cf5d0be
Aayush rai <148803304+Aayushraiofficial@users.noreply.github.com>
2023-10-24 06:23:14 +0530

    Add files via upload

---
+++ 20231010-011252.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main();
+{
+  int year;
+  clrscr();
+  printf("2005");
+  scanf("%d",& year);
+  if(year%4==0)
+  {
+   printf("leap year");
+   }
+   else
+   {
+    printf("leap year or not");
+    }
+    getch();
+    }
